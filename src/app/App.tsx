8d8ffcd96036b76b59345fa88ce021147ab218e6
lajanzen <lara-janzen@hotmail.com>
2021-06-01 09:08:37 +0200

    Delete constant

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,8 +2,6 @@ import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 function App(): JSX.Element {
-  const [count, setCount] = useState<number>(0);
-
   return (
     <BrowserRouter>
       <Switch>
